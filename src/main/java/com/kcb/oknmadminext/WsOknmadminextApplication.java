522e54ed1c5ce8c8d48f5e1400759b78c870fa6d
JungYoungsu <terun707@naver.com>
2019-03-12 18:52:13 +0900

    Start

---
+++ src/main/java/com/kcb/oknmadminext/WsOknmadminextApplication.java
@@ -0,0 +1,13 @@
+package com.kcb.oknmadminext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsOknmadminextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsOknmadminextApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kcb/oknmadminext/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.kcb.oknmadminext.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+	
+
+}
