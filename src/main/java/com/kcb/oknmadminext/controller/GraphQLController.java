cb731e2eac715ae7e6e68e18df70403f657332d6
JungYoungsu <terun707@naver.com>
2019-06-24 08:10:42 +0900

    크로스도메인, graphQL operationName Null 처리

--- src/main/java/com/kcb/oknmadminext/controller/GraphQLController.java
+++ src/main/java/com/kcb/oknmadminext/controller/GraphQLController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +22,8 @@ import io.leangen.graphql.GraphQLSchemaGenerator;
 import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
 import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
 
+
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class GraphQLController {
 	private final GraphQL graphQL;
@@ -40,10 +43,10 @@ public class GraphQLController {
 	
 	@PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public Map<String, Object> graphql(@RequestBody Map<String, String> request, HttpServletRequest raw) {
+    public Map<String, Object> graphql(@RequestBody Map<String, Object> request, HttpServletRequest raw) {
         ExecutionResult executionResult = graphQL.execute(ExecutionInput.newExecutionInput()
-                .query(request.get("query"))
-                .operationName(request.get("operationName"))
+                .query(request.get("query").toString())
+                .operationName(request.get("operationName") == null ? null : request.get("operationName").toString() )
                 .context(raw)
                 .build());
         return executionResult.toSpecification();
